c410928af25d58242c3f45777af67ad045a55bb3
Stephan Svoboda <stephan.svoboda@ebcont.com>
2021-12-10 10:45:35 +0100

    Catch stdout in test

--- tests/test_show_todos.py
+++ tests/test_show_todos.py
@@ -1,2 +1,10 @@
+import sys
+from io import StringIO
+
+
 def test_test():
-    assert False == True
+    sys.stdout = StringIO()
+    expected = "Hello World"
+    print("Hello World", end="")
+    actual = sys.stdout.getvalue()
+    assert expected == actual, f"Should {expected=} have been {actual=}"
