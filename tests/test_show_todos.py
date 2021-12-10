da233c13c8f36e45bca6f1b3c91cd6c603329ba3
Stephan Svoboda <stephan.svoboda@ebcont.com>
2021-12-10 12:45:59 +0100

    First real failing e2e test

--- tests/test_show_todos.py
+++ tests/test_show_todos.py
@@ -1,8 +1,9 @@
 import subprocess
 
 
-def test_test():
-    expected = "Hello World"
-    proc = subprocess.run(["startup.cmd", expected], stdout=subprocess.PIPE, check=True, universal_newlines=True)
+def test_show_list():
+    parameter = "list"
+    expected = "There are no todos!"
+    proc = subprocess.run(["startup.cmd", parameter], stdout=subprocess.PIPE, check=True, universal_newlines=True)
     actual = proc.stdout.replace("\x1b[0m", "")
     assert actual == f"{expected}\n", f"Should {expected=} have been {actual=}"
